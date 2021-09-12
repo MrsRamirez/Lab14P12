c66fa03b7dd3a7d6828dde673befac8c77d4bcef
MrsRamirezRamir <Mrs.ramirezq502@gmail.com>
2021-09-12 17:17:58 +0000

    Initial commit

---
+++ pt.js
@@ -0,0 +1,26 @@
+var student ={
+  fn:"";
+  ln:"";
+  bd:"";
+  ec:"";
+   sp:"";
+};
+/*
+var temp;*/
+function populateOjt(){
+  let student={
+  "fn":"document.listForm.fn.value;"
+  "ln":"document.listForm.ln.value;"
+  "bd":"document.listForm.bd.value;"
+  "ec":"document.listForm.ec.value;"
+  "sp":"document.listForm.sp.value;"
+  console.log(student);
+   }
+
+  /*function populate(student){
+   
+  temp= student{};
+  lunaStudentsList.push(temp);
+  document.getElementById("arrayDemo").innerHTML=lunaStudentsList + "<br>";*/
+
+
